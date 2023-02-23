22821b5da1ca4494858136bd4637e04bc9b109fc
AbishuaSR <123636307+AbishuaSR@users.noreply.github.com>
2023-02-23 11:07:18 +0530

    Add files via upload

---
+++ day2/Computer.java
@@ -0,0 +1,8 @@
+package week4.day2;
+
+public class Computer {
+	   public void computerModel(String modelName) {
+    	   System.out.println("The Computer model is " + modelName);
+       }
+
+}
---
+++ day2/Desktop.java
@@ -0,0 +1,13 @@
+package week4.day2;
+
+public class Desktop extends Computer {
+	 public void desktopSize(String desktopSize) {
+     	System.out.println("The Desktop size is " + desktopSize);
+     }
+     
+     public static void main(String[] args) {
+			Desktop desk = new Desktop();
+			desk.computerModel("Macintosh");
+			desk.desktopSize("Height 35cm, Width 24cm, Depth 28cm, Mass 7.5kg ");
+		}
+}
